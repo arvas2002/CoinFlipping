91e01ec08e43c0f0f035a0a4347075ae8fa14b6c
arvas2002 <arvas2002@gmail.com>
2022-05-02 14:45:45 +0200

    Homework Update
    Junit Test

--- src/test/java/coins/state/CoinsTest.java
+++ src/test/java/coins/state/CoinsTest.java
@@ -27,69 +27,69 @@ class CoinsTest {
     }
 
     @Test
-    void canFlipTest() {
-        BitSet bitSet = new BitSet(6);
+    void canBeFlipedTest() {
+        BitSet bitSet = new BitSet(8);
         assertFalse(state1.canFlip(bitSet));
-        bitSet.set(0, 3);
+        bitSet.set(0, 2);
         assertTrue(state1.canFlip(bitSet));
         assertTrue(state2.canFlip(bitSet));
-        bitSet = new BitSet(8);
+        bitSet = new BitSet(6);
         assertFalse(state1.canFlip(bitSet));
         assertFalse(state1.canFlip(bitSet));
-        bitSet = new BitSet(4);
-        bitSet.set(0, 3);
-        assertTrue(state1.canFlip(bitSet));
-        assertTrue(state2.canFlip(bitSet));
         bitSet = new BitSet(5);
-        bitSet.set(1, 2);
+        bitSet.set(0, 5);
+        assertTrue(state1.canFlip(bitSet));
+        assertTrue(state2.canFlip(bitSet));
+        bitSet = new BitSet(4);
+        bitSet.set(1, 4);
         assertFalse(state1.canFlip(bitSet));
         assertFalse(state2.canFlip(bitSet));
     }
 
     @Test
-    void IfCanBeFlipedBeFalseIfLengthIsLessThanNOrCardinalityIsLowerThanMTest() {
+    void IfCanBeFlipedIsFalseAndIfLengthIsLessThanNOrCardinalityIsLessThanMTest() {
         BitSet bitSet = new BitSet(10);
         assertFalse(state1.canFlip(bitSet));
-        bitSet = new BitSet(7);
-        bitSet.set(2);
+        bitSet = new BitSet(6);
+        bitSet.set(1);
         assertFalse(state1.canFlip(bitSet));
     }
 
     @Test
     void flip() {
-        BitSet bitSet = new BitSet(4);
-        bitSet.set(0, 3);
-        state1 = new Coins(7, 3);
+        BitSet bitSet = new BitSet(5);
+        bitSet.set(0, 4);
+        state1 = new Coins(7, 2);
         state1.flip(bitSet);
         assertEquals(bitSet, state1.getCoins());
-        BitSet bitSet2 = new BitSet(4);
-        bitSet2.set(0, 2);
+        BitSet bitSet2 = new BitSet(3);
+        bitSet2.set(0, 1);
         state1.flip(bitSet2);
         BitSet bitSet3 = new BitSet(4);
-        bitSet3.set(2);
+        bitSet3.set(3);
         assertEquals(bitSet3, state1.getCoins());
     }
 
     @Test
     void generateFlipsTest() {
-        long pieces = CombinatoricsUtils.binomialCoefficient(15, 8);
+        long pieces = CombinatoricsUtils.binomialCoefficient(13, 9);
         List<BitSet> flips = Coins.generateFlips(15, 8);
         assertEquals((int) pieces, flips.size());
-        pieces = CombinatoricsUtils.binomialCoefficient(8, 3);
+        pieces = CombinatoricsUtils.binomialCoefficient(7, 2);
         flips = Coins.generateFlips(8, 3);
         assertEquals((int) pieces, flips.size());
     }
 
     @Test
     void generateFlipsThrowingExceptionTest() {
-        assertThrows(IllegalArgumentException.class, () -> Coins.generateFlips(0, 2));
-        assertThrows(IllegalArgumentException.class, () -> Coins.generateFlips(2, 0));
-        assertThrows(IllegalArgumentException.class, () -> Coins.generateFlips(5, 6));
+        assertThrows(IllegalArgumentException.class, () -> Coins.generateFlips(0, 7));
+        assertThrows(IllegalArgumentException.class, () -> Coins.generateFlips(10, 0));
+        assertThrows(IllegalArgumentException.class, () -> Coins.generateFlips(4, 5));
     }
 
     @Test
     void getFlips() {
-        List<BitSet> flips = Coins.generateFlips(7, 3);
+        List<BitSet> flips = Coins.generateFlips(7, 2);
         assertEquals(flips.size(), state1.getFlips().size());
     }
 }
